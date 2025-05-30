0df3bb92ac834e67a74b7b1c79ace0ea8db175c7
Bruno Carvalho <brunobzs@gmail.com>
2025-05-30 13:36:01 -0300

    Add readme

--- cypress/e2e/trello-test.cy.js
+++ cypress/e2e/trello-test.cy.js
@@ -2,68 +2,46 @@ describe('Testes de API do Trello', () => {
   it('Deve criar um novo board', () => {
     const boardName = 'Meu Board de Teste';
 
-    cy.request({
-      method: 'POST',
-      url: `/boards/?name=${boardName}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-    }).then(response => {
+    cy.request('POST', `/boards/?name=${boardName}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(response => {
       expect(response.status).to.eq(200);
       expect(response.body.name).to.eq(boardName);
     });
   });
 
   it('Deve criar um novo card em um board', () => {
-    let boardId, listId;
     const listName = 'Minha Lista de Teste';
 
     cy.getMyBoards().then(responseBoards => {
-      boardId = responseBoards[0].id; // Pega o ID do Board criado no teste anterior
-    }).then(() => {
+      const boardId = responseBoards[0].id; // Pega o ID do Board criado no teste anterior
+
       // Cria uma nova lista no board
-      cy.request({
-        method: 'POST',
-        url: `/lists?name=${listName}&idBoard=${boardId}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-      }).then(responseList => {
-        listId = responseList.body.id; // Pega o ID da lista criada
+      cy.request('POST', `/lists?name=${listName}&idBoard=${boardId}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(responseList => {
+        const listId = responseList.body.id; // Pega o ID da lista criada
 
         // Cria um novo card na lista
-        cy.request({
-          method: 'POST',
-          url: `/cards?idList=${listId}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-        }).then(responseCard => {
-          console.log(responseCard)
+        cy.request('POST', `/cards?idList=${listId}&key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(responseCard => {
           expect(responseCard.status).to.eq(200);
           expect(responseCard.body.idList).to.eq(listId);
           expect(responseCard.body.idBoard).to.eq(boardId);
         })
       })
-    });
+    })
   });
 
   it('Deve excluir um card existente', () => {
-    let boardId, listId, cardId;
-
     cy.getMyBoards().then(responseBoards => {
-      boardId = responseBoards[0].id; // Pega o ID do Board criado no teste anterior
+      const boardId = responseBoards[0].id; // Pega o ID do Board criado no teste anterior
 
       // Pega as listas do board
-      cy.request({
-        method: 'GET',
-        url: `/boards/${boardId}/lists?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-      }).then(responseLists => {
-        listId = responseLists.body[0].id; // Pega o ID da primeira lista do board
+      cy.request('GET', `/boards/${boardId}/lists?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(responseLists => {
+        const listId = responseLists.body[0].id; // Pega o ID da primeira lista do board
 
         // Pega os cards da lista
-        cy.request({
-          method: 'GET',
-          url: `/lists/${listId}/cards?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-        }).then(responseCards => {
-          cardId = responseCards.body[0].id; // Pega o ID do primeiro card da lista
+        cy.request('GET', `/lists/${listId}/cards?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(responseCards => {
+          const cardId = responseCards.body[0].id; // Pega o ID do primeiro card da lista
 
           // Exclui o card
-          cy.request({
-            method: 'DELETE',
-            url: `/cards/${cardId}?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-          }).then(responseCards => {
+          cy.request('DELETE', `/cards/${cardId}?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(responseCards => {
             expect(responseCards.status).to.eq(200);
           })
         })
@@ -72,16 +50,11 @@ describe('Testes de API do Trello', () => {
   });
 
   it('Deve excluir um board existente', () => {
-    let boardId;
-
     cy.getMyBoards().then(response => {
-      boardId = response[0].id;
+      const boardId = response[0].id;
 
       // Exclui o board
-      cy.request({
-        method: 'DELETE',
-        url: `/boards/${boardId}?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-      }).then(response => {
+      cy.request('DELETE', `/boards/${boardId}?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(response => {
         expect(response.status).to.eq(200);
       });
     })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,5 @@
 Cypress.Commands.add('getMyBoards', () => {
-  return cy.request({
-    method: 'GET',
-    url: `/members/me/boards?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`
-  }).then(response => {
+  return cy.request('GET', `/members/me/boards?key=${Cypress.env('API_KEY')}&token=${Cypress.env('TOKEN')}`).then(response => {
     return response.body.filter(board => board.name === 'Meu Board de Teste');
   })
 })
